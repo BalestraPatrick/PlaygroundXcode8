b7a3e5282243acf04722855411837063790faed5
Eliott hauteclair <me@eliott.be>
2017-12-29 17:15:50 +0100

    Swift 4
    Swift 4 migration

--- AsyncDemo.playground/Contents.swift
+++ AsyncDemo.playground/Contents.swift
@@ -5,7 +5,7 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 let url = URL(string: "https://emergency-phone-numbers.herokuapp.com")!
-let session = URLSession.shared()
+let session = URLSession.shared
 let q = session.dataTask(with: url) { data, response, error in
     do {
         if let d = data, let dictionary = try JSONSerialization.jsonObject(with: d, options: []) as? [String: AnyObject] {
